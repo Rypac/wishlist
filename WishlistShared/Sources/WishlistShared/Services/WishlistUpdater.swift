d6ead399cba24863b1762769c5972284f7c3d3f3
Ryan Davis <ryan@rypac.org>
2020-04-03 20:35:37 +1100

    Only save apps which have updates

--- WishlistShared/Sources/WishlistShared/Services/WishlistUpdater.swift
+++ WishlistShared/Sources/WishlistShared/Services/WishlistUpdater.swift
@@ -24,10 +24,22 @@ public final class WishlistUpdater {
       .setFailureType(to: Error.self)
       .flatMap { [appLookupService] apps in
         appLookupService.lookup(ids: apps.map(\.id))
+          .map { updatedApps in
+            updatedApps.reduce(into: [App]()) { result, updatedApp in
+              guard let app = apps.first(where: { $0.id == updatedApp.id }) else {
+                return
+              }
+              if updatedApp.updateDate > app.updateDate {
+                result.append(updatedApp)
+              }
+            }
+          }
       }
       .receive(on: DispatchQueue.main)
       .sink(receiveCompletion: { _ in }) { [wishlist] apps in
-        wishlist.update(apps: apps)
+        if !apps.isEmpty {
+          wishlist.update(apps: apps)
+        }
       }
       .store(in: &cancellables)
   }
