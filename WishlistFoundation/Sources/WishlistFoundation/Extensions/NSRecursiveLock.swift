33c5a53346f8cf0e21c8393b682985870cf442a2
Ryan Davis <ryan@rypac.org>
2020-04-13 13:49:05 +1000

    Inline recursive lock extensions

--- WishlistFoundation/Sources/WishlistFoundation/Extensions/NSRecursiveLock.swift
+++ WishlistFoundation/Sources/WishlistFoundation/Extensions/NSRecursiveLock.swift
@@ -1,12 +1,14 @@
 import Foundation
 
 public extension NSRecursiveLock {
+  @inline(__always)
   func synchronized(_ execute: () -> Void) {
     lock()
     defer { unlock() }
     execute()
   }
 
+  @inline(__always)
   func synchronized<T>(_ execute: () -> T) -> T {
     lock()
     defer { unlock() }
