74d35d020a984a11d0816c028c9b55591744b459
Ryan Davis <ryan@rypac.org>
2022-07-07 16:54:38 +1000

    Use new navigationDestination method

--- Modules/Sources/ToolboxUI/Navigation.swift
+++ Modules/Sources/ToolboxUI/Navigation.swift
@@ -1,38 +1,26 @@
 import SwiftUI
 
 extension View {
-  public func navigation<Destination: View>(
-    isActive: Binding<Bool>,
-    @ViewBuilder destination: @escaping () -> Destination
-  ) -> some View {
-    background {
-      NavigationLink(isActive: isActive, destination: destination, label: EmptyView.init)
-    }
-  }
-
-  public func navigation<Item: Identifiable, Destination: View>(
+  public func navigationDestination<Item: Identifiable, Destination: View>(
     item: Binding<Item?>,
     @ViewBuilder destination: @escaping (Item) -> Destination
   ) -> some View {
-    background {
-      NavigationLink(
-        isActive: Binding(
-          get: { item.wrappedValue != nil },
-          set: { value in
-            if !value {
-              item.wrappedValue = nil
-            }
+    navigationDestination(
+      isPresented: Binding(
+        get: { item.wrappedValue != nil },
+        set: { value in
+          if !value {
+            item.wrappedValue = nil
           }
-        ),
-        destination: {
-          if let item = item.wrappedValue {
-            destination(item)
-          } else {
-            EmptyView()
-          }
-        },
-        label: EmptyView.init
-      )
-    }
+        }
+      ),
+      destination: {
+        if let item = item.wrappedValue {
+          destination(item)
+        } else {
+          EmptyView()
+        }
+      }
+    )
   }
 }
